051190a9724e9e5b330128646153e0c4dd5e3d6c
Doug Hagan <dougecko@users.noreply.github.com>
2021-07-18 17:10:36 +1000

    add person endpoint, handle CORS

--- handler.js
+++ handler.js
@@ -3,6 +3,10 @@
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(
       {
         message: "Go Serverless v2.0! Your function executed successfully!",
@@ -11,5 +15,19 @@ module.exports.hello = async (event) => {
       null,
       2
     ),
+  };
+};
+
+module.exports.person = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      firstName: "John",
+      lastName: "Doe"
+    }),
   };
 };
